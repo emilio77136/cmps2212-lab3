648f0cdc9d3daf2520ebe85b794a0e316d32dba8
Emilio Lopez <2023158951@ub.edu.bz>
2026-02-15 19:46:04 -0600

    Complete Exercise 1: Multi-Button Highlight

---
+++ exercise1.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.color-btn');
+
+function changeColor(){
+    this.style.backgroundColor = "yellow";
+}
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", changeColor) ;  
+});
